03af3bdcfe1ba672abbfaad215e44ec11f0cef43
andrewwang <andrewwang@freedom.net.tw>
2023-06-30 11:39:09 +0800

    dhcp scan and snmp scan now run in port scan removed arp-scan warning message pp scan now only scans for udp port for better accuarcy

--- scanner.sh
+++ scanner.sh
@@ -6,33 +6,30 @@ mkdir lists &> /dev/null
 
 # IP and mask of local machine
 ip=$(hostname -I)	## grab ip using hostname -I
-mask=$(ip a | grep $ip | awk '{print $2}')	## grab network mask using ip ex: "127.xxx.xxx.xxx/xx"
+mask=$(ip a | grep $ip | awk '{print $2}')	## grab network mask using ip
 
 # Responsive IPs and MAC Addresses
 echo "Status: Initiating ARP scan..."
-arp-scan -q -x $mask | awk '{print $1}' | sort -u > ./lists/ip_list.txt	## arp-scan to pull ipv4 and mac from cache and store it in ip_list
+arp-scan -q -x $(echo $mask | sed 's/\.[0-9]*\//\.0\//g') | awk '{print $1}' | sort -u > ./lists/ip_list.txt	## arp-scan to pull ipv4 and mac from cache and store it in ip_list
+echo "Status: ARP scan done."
 echo "Status: Initiating ping scan..."
 nmap -sn $mask --min-parallelism 10000 -T5 --host-timeout 5ms -oX ./log/ping_log.xml &> /dev/null	## ping scan the network for working ipv4 and mac, results stored in xml
 echo "Status: Ping scan finished."
 xmllint --xpath '//host/address[@addrtype="ipv4"]/@addr' ./log/ping_log.xml | cut -d '"' -f 2 >> ./lists/ip_list.txt	## parse xml file for ip and store it in ip_list.txt
 sort -o ./lists/ip_list.txt -u ./lists/ip_list.txt	## sort out duplicate ip
 
+# SMNP
+snmp_scan(){
+	echo "Status: Performing SNMP scan..."
+	nmap -sU -p161 --script=snmp-info -iL ./lists/udp_list.txt --min-parallelism 10000 -T5 -oX ./log/snmp_log.xml &> /dev/null	## scan port 161(snmp) using script, results stored in xml
+	echo "Status: SNMP scan done."
+}
+
 # DHCP
 dhcp_scan(){
-	dhcp_ip=$(journalctl -r | grep -i "dhcpack" | grep $(hostname) | awk '{print $10}')	## get dhcp server ip using journalctl
-	if [[ -z "$dhcp_ip" ]]	## if ip found then proceed
-	then
-    	echo "Warning: No DHCP server found"
-	else
-    	echo "Status: Scanning DHCP server..."
-		nmap -sU --script=dhcp-discover $dhcp_ip --min-parallelism 10000 -T5 -oX ./log/dhcp_log.xml &> /dev/null	## scan dhcp server ip with script, results stored in xml
-		echo "Status: DHCP scan done".
-		### check if dhcp server is in another network
-		#if [[ ! $(grep $dhcp_ip ./lists/ip_list.txt) ]]
-		#then
-    		## run a scan in the network
-		#fi
-	fi
+	echo "Status: Performing DHCP scan..."
+	nmap -sU -p67-68 --script=dhcp-discover -iL ./lists/udp_list.txt --min-parallelism 10000 -T5 -oX ./log/dhcp_log.xml &> /dev/null	## scan port 67(dhcp) using script, results stored in xml
+	echo "Status: DHCP scan done."
 }
 
 # Ports and Services
@@ -40,10 +37,12 @@ port_scan(){
 	echo "Status: Probing for open ports..."
 	nmap -sS -iL ./lists/ip_list.txt --min-parallelism 10000 -T5 -oX ./log/tcp_log.xml &> /dev/null &	## tcp port scan ip_list, results stored in xml
 	### check what port protocols ip use
-	nmap -sO -iL ./lists/ip_list.txt --min-parallelism 10000 -T5 -oX ./log/pp_log.xml &> /dev/null &	## port protocol scan ip_list to see if ip uses udp, results stored in xml
+	nmap -sO -p17 -iL ./lists/ip_list.txt --min-parallelism 10000 -T5 -oX ./log/pp_log.xml &> /dev/null &	## port protocol scan ip_list to see if ip uses udp, results stored in xml
 	wait
-	xmllint --xpath '//service[@name="udp"] | //address[@addrtype="ipv4"]' ./log/pp_log.xml | grep -B 1 "udp" | grep -i "ipv4" | cut -d '=' -f 2 | cut -d '"' -f 2 > ./lists/udp_list.txt 
+	xmllint --xpath '//address[@addrtype="ipv4"] | //port' ./log/pp_log.xml | grep -B 1 "open" | grep -i "ipv4" | cut -d '"' -f 2 > ./lists/udp_list.txt 
 		## line above parses pp_log to search for IPs that uses UDP and stores it in udp_list 
+	snmp_scan &
+	dhcp_scan &	## run snmp and dhcp scan once udp_list is formed
 	nmap -sU -iL ./lists/udp_list.txt --min-parallelism 10000 -T5 -oX ./log/udp_log.xml &> /dev/null 	## udp port scan udp_list, results stored in xml
 	echo "Status: Ports and services acquired."
 }
@@ -55,7 +54,7 @@ os_scan(){
 	echo "Status: OS scan done."
 }
 
-dhcp_scan &
+
 port_scan &
 os_scan &
 wait
